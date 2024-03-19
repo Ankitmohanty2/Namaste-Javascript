193c6a86f978f55506e2d67f33e62cdc32e04cf6
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-20 00:36:50 +0530

    its just the code

---
+++ index.js
@@ -0,0 +1,11 @@
+var n = 2;
+function square(num){
+    var ans = num * num;
+    return ans;
+}
+
+var squareNum = square(n);
+var squareNum2 = square(4);
+
+console.log(squareNum);
+console.log(squareNum2);
