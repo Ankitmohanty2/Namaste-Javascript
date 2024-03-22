8ba6148a19e1239a43c754b97a3928963300b888
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-22 16:35:06 +0530

    why its calling a function?

--- Hoisting/index.js
+++ Hoisting/index.js
@@ -1,3 +1,10 @@
+
+
+// getName(); // it will give error as getName is not defined yet
+// console.log(x); // it will give undefined as x is not defined yet
+// console.log(getName); // it will give the function definition
+
+
 var x = 7;
 
 function getName() {
