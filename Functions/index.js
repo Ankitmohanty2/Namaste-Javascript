2ac03bda06d1c53610c52796524e22ea50839c13
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-24 21:54:15 +0530

    done

--- Functions/index.js
+++ Functions/index.js
@@ -1,10 +1,17 @@
+var x = 1;
+a();
+b();
+console.log(x);
+
+
+
 function a(){
-    let x = 10;
+    var x = 10;
     console.log(x);
 }
 
 
 function b(){
-    let x = 100;
+    var x = 100;
     console.log(x);
 }
