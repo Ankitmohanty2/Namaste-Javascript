4d2a8966d64d6d51f671e10345301284cebbdcc3
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-26 23:21:21 +0530

    it was good

--- important/destructuring.js
+++ important/destructuring.js
@@ -1,4 +1,6 @@
+// Destructuring is a way to extract values from objects and arrays and bind them to variables in a more concise way.
+
 let arr = [67, 42, 54, 23, 12, 98, 34, 56, 78];
-//let [a, b, c, d, ...rest] = arr;
 let [a, , b, c, d, ...rest] = arr;
+
 console.log(a, b, c, d, rest);
