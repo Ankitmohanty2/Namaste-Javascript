0930b6997f50134296e523fd91abdfdbdec64ddf
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-29 17:58:14 +0530

    done

--- Scope/scope.js
+++ Scope/scope.js
@@ -8,3 +8,9 @@ function a() {
 
 a();
 console.log(b);
+
+
+//Scope is the context in which a variable is declared. In JavaScript, variables have either global or function scope.
+// In the example above, the variable b is declared inside the function a, so it is only accessible within that function.
+// When the function a is called, the variable b is declared and initialized to 10, and then the function c is called.
+// The function c is declared inside the function a, so it has access to the variable b. However, the variable b is not accessible outside of the function a, so the console.log(b) statement will throw an error.
