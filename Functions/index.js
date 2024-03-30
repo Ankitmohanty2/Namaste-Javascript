69e9a5b171615d2deb6366ae881cf4be8cb14c13
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-30 21:39:14 +0530

    higher order function

--- Functions/index.js
+++ Functions/index.js
@@ -15,3 +15,18 @@ function b(){
     var x = 100;
     console.log(x);
 }
+
+
+// Higher Order Functions
+
+let name = function(name, age){
+    for(let i = 0; i < age; i++){
+        name();
+    }
+}
+
+let printName = function(){
+    console.log("John Doe");
+}
+
+name(printName, 5);
