299ea0d35310e0c35ba2a121744952d16fef4cb6
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-30 21:55:32 +0530

    forEach method

---
+++ Array/index.js
@@ -0,0 +1,15 @@
+// forEach Method
+
+let arr = [1, 2, 3, 4, 5];
+
+function print (value) {
+    console.log(value);
+}
+
+//1- arr.forEach(print);
+
+//Or
+
+// 2- arr.forEach(function(value){
+   // console.log(value);
+ //});
