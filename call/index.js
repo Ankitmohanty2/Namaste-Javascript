46b8977e7e5fb97d5532208c373967f0cbbf273c
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-31 15:46:24 +0530

    apply method

--- call/index.js
+++ call/index.js
@@ -1,15 +1,20 @@
 let name1 = {
     firstName: "virat",
-    lastName: "kohli",
-printName : function(){
-    console.log(this.firstName + " " + this.lastName);
+    lastName: "kohli"
 }
+let printName = function(hometown, state){
+    console.log(this.firstName + " " + this.lastName  + " from " +  hometown + " " + state);
 }
-name1.printName(); 
+// function borrowing
+// in call method we pass these arguments individually or comma separated
+printName.call(name1, "Delhi", "uttar pradesh"); 
 
 
 let name2 = {
     firstName: "rohit",
     lastName: "sharma",
 }
-name1.printName.call(name2); //call method is used to call the function of another object
+
+
+//in apply method we pass these arguments in array
+printName.apply(name2, ["Mumbai", "maharastra"]); //apply method is used to call the function of another object
