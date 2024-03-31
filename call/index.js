3fa2e1e9be0c73bee06209cba21fd596b079579d
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-03-31 15:48:40 +0530

    bind mthod

--- call/index.js
+++ call/index.js
@@ -18,3 +18,7 @@ let name2 = {
 
 //in apply method we pass these arguments in array
 printName.apply(name2, ["Mumbai", "maharastra"]); //apply method is used to call the function of another object
+
+//bind method is used to bind the function to the object
+let printMyName = printName.bind(name1, "Delhi", "uttar pradesh");
+printMyName(); 
