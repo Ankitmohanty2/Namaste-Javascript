9cdac0685f742d686814d2b347ff8066bf2c3904
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-01 07:45:25 +0530

    done currying

--- currying/index.js
+++ currying/index.js
@@ -4,8 +4,11 @@ let multiply = function(x, y){
     console.log(x * y);
 }
 
-let multiplyByTwo = multiply.bind(this, 2);
-multiplyByTwo(5);
+let multiplyByTwo = multiply.bind(this, );
+multiplyByTwo(7,5);
 
 let multiplyByThree = multiply.bind(this, 3);
 multiplyByThree(5);
+
+
+//so currying is a process of converting a function that takes multiple arguments into a function that takes them one at a time.
