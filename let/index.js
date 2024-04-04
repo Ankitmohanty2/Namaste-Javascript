13afca70c3508b2a326c2b935cfeebe8e4a42486
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-04 22:09:08 +0530

    temporal dead zone

--- let/index.js
+++ let/index.js
@@ -1,4 +1,8 @@
-console.log(b);
+// console.log(b);
 
 let a = 10;
+console.log(a);
 var b =100;
+
+//Temporal Dead Zone
+//The Temporal Dead Zone (TDZ) is the period between the start of the block scope and the actual declaration of the variable.
