234bd771d8b3e2a6ff4f3b6d7d652d1cb11c2881
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-04 23:09:55 +0530

    done shadowing

--- block/index.js
+++ block/index.js
@@ -5,18 +5,22 @@
 
 // }
 
-
+var a = 200;
+let b = 300; //script scoped
 {
-    var a = 10;
+    var a = 10; //here a is shadowed
     let b = 20;
     const c = 30;
     console.log(a); // 10
     console.log(b); // 20
     console.log(c); // 30
 }
+console.log(b); // 300
 
 console.log(a); // 10
 console.log(b); // ReferenceError: b is not defined
 console.log(c); // ReferenceError: c is not defined
 
 // reason is var is global/function scoped and let and const are block scoped
+
+//you cant shadow a variable with let or const
