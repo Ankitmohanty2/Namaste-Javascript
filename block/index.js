4afbe668218f67b3af9431967ebd4c5ba748b1c1
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-04 22:49:03 +0530

    again scope

--- block/index.js
+++ block/index.js
@@ -1,6 +1,22 @@
-if(true) {
-    // compound statement
-    var a = 10;
-    console.log(a);
+// if(true) {
+//     // compound statement
+//     var a = 10;
+//     console.log(a);
 
+// }
+
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a); // 10
+    console.log(b); // 20
+    console.log(c); // 30
 }
+
+console.log(a); // 10
+console.log(b); // ReferenceError: b is not defined
+console.log(c); // ReferenceError: c is not defined
+
+// reason is var is global/function scoped and let and const are block scoped
