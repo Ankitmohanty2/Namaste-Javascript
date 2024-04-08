d0f2ceff5bab7406eb7db662f98a4e843586f5da
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-08 12:56:30 +0530

    normal function

--- practice/index.js
+++ practice/index.js
@@ -1,9 +1,20 @@
 
 // By Creating Anonymous Function
+// let myValue = [78, 89, 90, 56, 42, 43, 56, 67];
+
+// let solve = myValue.filter(function(clear){
+//     return clear < 70;
+// });
+
+// console.log(solve);
+
+
+// By creating normal function
 let myValue = [78, 89, 90, 56, 42, 43, 56, 67];
 
-let solve = myValue.filter(function(clear){
-    return clear < 70;
-});
+function solve(back) {
+    return back < 70;
+}
 
-console.log(solve);
+let wako = myValue.filter(solve);
+console.log(wako);
