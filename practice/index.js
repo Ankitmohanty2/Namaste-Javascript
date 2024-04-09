e85f436d2d0646fbd2156c334a7da2545ef42264
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-09 21:41:50 +0530

    come on dude

--- practice/index.js
+++ practice/index.js
@@ -2,6 +2,8 @@
 // By Creating Anonymous Function
 // let myValue = [78, 89, 90, 56, 42, 43, 56, 67];
 
+const { ResourceLoader } = require("jsdom");
+
 // let solve = myValue.filter(function(clear){
 //     return clear < 70;
 // });
@@ -18,3 +20,17 @@ function solve(back) {
 
 let wako = myValue.filter(solve);
 console.log(wako);
+
+
+async function greet() {
+    return "hello world!";
+}
+
+
+greet()
+.then((Result)=>{
+console.log("solution is good");
+})
+.catch((err) => {
+    console.log("its an error");
+})
