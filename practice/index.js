a24c7a0e4102fbf5d37026315b977bf3b2cb8328
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-10 22:02:05 +0530

    comment

--- practice/index.js
+++ practice/index.js
@@ -12,25 +12,26 @@ const { ResourceLoader } = require("jsdom");
 
 
 // By creating normal function
-let myValue = [78, 89, 90, 56, 42, 43, 56, 67];
+// let myValue = [78, 89, 90, 56, 42, 43, 56, 67];
 
-function solve(back) {
-    return back < 70;
-}
+// function solve(back) {
+//     return back < 70;
+// }
 
-let wako = myValue.filter(solve);
-console.log(wako);
-
-
-async function greet() {
-    return "hello world!";
-}
+// let wako = myValue.filter(solve);
+// console.log(wako);
 
 
-greet()
-.then((Result)=>{
-console.log("solution is good");
-})
-.catch((err) => {
-    console.log("its an error");
-})
+// async function greet() {
+//     return "hello world!";
+// }
+
+
+// greet()
+// .then((Result)=>{
+// console.log("solution is good");
+// })
+// .catch((err) => {
+//     console.log("its an error");
+// })
+
