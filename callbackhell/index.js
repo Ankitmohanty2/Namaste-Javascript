d56f66c2576b6169abc0a4c9a458b25051897898
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-11 12:48:15 +0530

    yup learning

--- callbackhell/index.js
+++ callbackhell/index.js
@@ -4,3 +4,8 @@
 setTimeout(function(){
     console.log("hello world");
 }, 3000);
+
+const cart = ["shoes", "shirt", "pant", "watch", "mobile"];
+cart.forEach(function(item){
+    console.log(item);
+});
