57f1de39425a5767267b900436062a9521dee86b
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-16 08:01:29 +0530

    not good but well

--- Array/map.js
+++ Array/map.js
@@ -1,16 +1,15 @@
 let map = [23, 78, 45, 8, 32, 56, 45, 78, 90, 12];
 
-
 //map returns an array of values that pass the condition
+//aur ek baat map hamesha ek array return karega uske liye return keyword use karna padega
 // let result = map.map(function(value){
 //     return value * 2;
 // });
 
-// console.log(result); 
+// console.log(result);
 
-
-let value = map.filter(function(value){
-    return value > 50;
+let value = map.filter(function (value) {
+  return value > 50;
 });
 
 console.log(value);
