b96379270643007e7fb19ea198bf35acc0a077bf
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-21 10:06:34 +0530

    function scope

--- oops/index.js
+++ oops/index.js
@@ -8,3 +8,20 @@ const user = {
 };
 
 console.log(user.getAgeYear());
+
+//encapsulation can be used in two ways in javascript
+//1. using function scope
+//2. closures
+
+//Function scope
+
+function messageFunct() {
+  const message = "Hey there!";
+  console.log("Message from function scope:", message);
+}
+
+//Calling the function which internally defines the message variable
+messageFunct();
+
+//Trying to access the message from outside the function which defines it
+console.log("Message from function scope:", message);
