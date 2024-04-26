9c1a028ca3ab5532721c0e3204abe1ce8a6fefb2
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-26 20:38:52 +0530

    with filter

--- polyfils/polyfils.js
+++ polyfils/polyfils.js
@@ -16,3 +16,23 @@ let result = map.myMap(function (value) {
 });
 
 console.log(result);
+
+// polyfill for filter
+
+Array.prototype.myFilter = function (callback) {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+};
+
+let filter = [23, 78, 45, 8, 32, 56, 45, 78, 90, 12];
+
+let result1 = filter.myFilter(function (value) {
+  return value > 30;
+});
+
+console.log(result1);
