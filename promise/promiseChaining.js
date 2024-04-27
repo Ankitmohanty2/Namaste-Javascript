889504c95989deec6f0c6eb666f2945b2d0b3246
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-27 14:04:55 +0530

    promise chaining

---
+++ promise/promiseChaining.js
@@ -0,0 +1,21 @@
+doSomething(function (result) {
+  doSomethingElse(
+    result,
+    function (newResult) {
+      doThirdThing(
+        newResult,
+        function (finalResult) {
+          console.log(`Got the final result: ${finalResult}`);
+        },
+        failureCallback
+      );
+    },
+    failureCallback
+  );
+}, failureCallback);
+
+//so promise chaining is used to avoid the callback hell
+
+doSomething();
+doSomethingElse();
+doThirdThing();
