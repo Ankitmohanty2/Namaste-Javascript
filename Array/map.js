d145d738afd2ec7c30fa62900ee37a2f22d445ff
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-04-29 19:37:19 +0530

    learning oops

--- Array/map.js
+++ Array/map.js
@@ -8,11 +8,11 @@ let map = [23, 78, 45, 8, 32, 56, 45, 78, 90, 12];
 
 // console.log(result);
 
-let value = map.filter(function (value) {
-  return value > 50;
-});
+// let value = map.filter(function (value) {
+//   return value > 50;
+// });
 
-console.log(value);
+// console.log(value);
 
 //filter returns an array of values that pass the condition True or False
 
@@ -24,16 +24,16 @@ console.log(value);
 // });
 // console.log(extra);
 
-let big = [11, 22, 3, 4, 5, 6, 7, 8, 9, 10];
-let small = big.map((elem) => (elem > 5 ? elem + 12 : elem));
-console.log(small);
+// let big = [11, 22, 3, 4, 5, 6, 7, 8, 9, 10];
+// let small = big.map((elem) => (elem > 5 ? elem + 12 : elem));
+// console.log(small);
 
 // Filter
-let array3 = [
-  { name: "batman", gender: "Male" },
-  { name: "wonderwoman", gender: "Female" },
-  { name: "spiderman", gender: "Male" },
-];
+// let array3 = [
+//   { name: "batman", gender: "Male" },
+//   { name: "wonderwoman", gender: "Female" },
+//   { name: "spiderman", gender: "Male" },
+// ];
 
-let filter = array3.filter((elem) => elem.gender === "Male");
-console.log(filter);
+// let filter = array3.filter((elem) => elem.gender === "Male");
+// console.log(filter);
