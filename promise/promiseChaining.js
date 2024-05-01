2fc83de3c25a997f8f2ca65107f1a1ba96770776
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-05-01 20:04:08 +0530

    some mistakes

--- promise/promiseChaining.js
+++ promise/promiseChaining.js
@@ -1,21 +1,12 @@
-doSomething(function (result) {
-  doSomethingElse(
-    result,
-    function (newResult) {
-      doThirdThing(
-        newResult,
-        function (finalResult) {
-          console.log(`Got the final result: ${finalResult}`);
-        },
-        failureCallback
-      );
-    },
-    failureCallback
-  );
-}, failureCallback);
-
-//so promise chaining is used to avoid the callback hell
-
-doSomething();
-doSomethingElse();
-doThirdThing();
+const flipACoin = new Promise((resolve, reject) => {
+  resolve();
+})
+  .then(() => {
+    doSomething();
+  })
+  .then(() => {
+    doSomethingElse();
+  })
+  .then(() => {
+    doAnotherThing();
+  });
