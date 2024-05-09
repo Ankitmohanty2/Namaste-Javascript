c3fe4aedcd1a53d99352fd0640796a66e629a2fe
Ankitmohanty2 <mohantyankit619@gmail.com>
2024-05-09 13:31:14 +0530

    added find method

---
+++ Array/find.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5];
+
+// Find the first element that is greater than 3
+const result = numbers.find((element) => element > 3);
+
+console.log(result); // Output: 4
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const result = numbers.find((element) => element > 10);
+
+// console.log(result); // Output: undefined
