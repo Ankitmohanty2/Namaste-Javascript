20418f752a0f6945d6f3c8778995958713e7ba48
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2024-06-03 13:08:44 +0530

    Update index.js

--- Higherorder/index.js
+++ Higherorder/index.js
@@ -1,12 +1,16 @@
-// Callback function, passed as a parameter in the higher order function
+// Define the callback function
 function callbackFunction() {
-  console.log("I am  a callback function");
+  console.log("I am a callback function");
 }
 
-// higher order function
-function higherOrderFunction(func) {
-  console.log("I am higher order function");
-  func();
+// Define the higher order function that takes a function as a parameter
+function higherOrderFunction(callback) {
+  // Log a message indicating that this is a higher order function
+  console.log("I am a higher order function");
+  
+  // Call the provided callback function
+  callback();
 }
 
+// Call the higher order function and pass the callback function as an argument
 higherOrderFunction(callbackFunction);
